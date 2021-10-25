d7598bbeafd327c4222a5fe827a8377bf169c828
Geomar Perales Apaico <per3app@gmail.com>
2021-10-25 00:19:04 -0500

    Add files via upload

---
+++ R/Andahuaylas.R
@@ -0,0 +1,7 @@
+#' A dataset of historical precipitation and information about Andahuaylas Station
+#'
+#' A dataset of historical precipitation and information about Andahuaylas Station (ANA, Perú).
+#' @source data downloaded from ANA (Peru), these data being distributed without any warranty.
+#'
+"Andahuaylas"
+
